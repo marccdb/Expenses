aaaafd127af586aafeb303c03ea0b33c4e07cccf
Marcio Del Bianco <marcio.bianco@gmail.com>
2022-06-28 17:48:59 -0300

    add controller and model fixes

---
+++ ExpensesAPI/Controllers/ExpensesController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ExpensesAPI.Data;
+using ExpensesAPI.Models;
+
+namespace ExpensesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        private readonly ExpensesContext _context;
+
+        public ExpensesController(ExpensesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Expenses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Expense>>> GetExpenses()
+        {
+          if (_context.Expenses == null)
+          {
+              return NotFound();
+          }
+            return await _context.Expenses.ToListAsync();
+        }
+
+        // GET: api/Expenses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Expense>> GetExpense(int id)
+        {
+          if (_context.Expenses == null)
+          {
+              return NotFound();
+          }
+            var expense = await _context.Expenses.FindAsync(id);
+
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            return expense;
+        }
+
+        // PUT: api/Expenses/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutExpense(int id, Expense expense)
+        {
+            if (id != expense.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(expense).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExpenseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Expenses
+        [HttpPost]
+        public async Task<ActionResult<Expense>> PostExpense(Expense expense)
+        {
+          if (_context.Expenses == null)
+          {
+              return Problem("Entity set 'ExpensesContext.Expenses'  is null.");
+          }
+            _context.Expenses.Add(expense);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetExpense", new { id = expense.Id }, expense);
+        }
+
+        // DELETE: api/Expenses/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExpense(int id)
+        {
+            if (_context.Expenses == null)
+            {
+                return NotFound();
+            }
+            var expense = await _context.Expenses.FindAsync(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            _context.Expenses.Remove(expense);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ExpenseExists(int id)
+        {
+            return (_context.Expenses?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- ExpensesAPI/Models/Expense.cs
+++ ExpensesAPI/Models/Expense.cs
@@ -1,14 +1,21 @@
 ﻿using ExpensesAPI.Models.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ExpensesAPI.Models
 {
     public class Expense
     {
         public int Id { get; set; }
+        [Required]
         public string ExpenseName { get; set; }
+        [Required]
         public string EstablishmentName { get; set; }
+
         public DateTime Date { get; set; }
+        
         public ExpenseCategory Category { get; set; }
+        [Required]
         public double Amount { get; set; }
 
 
